a86e838aa576d7e7e4088214594eeb980ab8b1bb
Mike Rousos <Mike@MJRousos.com>
2018-12-13 17:46:30 -0500

    Add initial host and target source and build files for CoreCLRHost sample

---
+++ HostWithCoreclrhost/src/ManagedLibrary/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ManagedLibrary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ HostWithCoreclrhost/src/SampleHost.cpp
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <iostream>
+#include "coreclrhost.h"
+using namespace std;
+
+int main()
+{
+    printf("Hello, world!\n");
+    cout << "Hello again!" <<endl;
+    return 0;
+}
